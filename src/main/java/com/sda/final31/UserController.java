22a480d28dbc21aa4aa45d96b3004030fb620d6a
kf <napewnoniepodam@gmail.com>
2022-02-27 13:01:24 +0100

    Rejestracja

--- src/main/java/com/sda/final31/UserController.java
+++ src/main/java/com/sda/final31/UserController.java
@@ -1,16 +1,24 @@
 package com.sda.final31;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping("/register")
+    public String displayForm(Model model) {
+        model.addAttribute("aaaatxt", "Napis " + System.currentTimeMillis());
+        return "registerPage";
+    }
+
     @PostMapping("/register")
     public String register(@ModelAttribute UserRegistrationDto userRegistrationDto) {
 
 
-        return ""; //nazwa pliku html
+        return "registerPage"; //nazwa pliku html
     }
 }
