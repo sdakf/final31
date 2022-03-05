6d8b650a0ed9d997d78b781f81bb46b830b60712
kf <napewnoniepodam@gmail.com>
2022-03-05 14:22:43 +0100

    Categories finished

--- src/main/java/com/sda/final31/finalapp/categories/CategoryController.java
+++ src/main/java/com/sda/final31/finalapp/categories/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CategoryController {
@@ -12,8 +13,8 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/categories")
-    public String showCategories(Model model) {
-        model.addAttribute("categoriesData", categoryService.findCategories());
+    public String showCategories(@RequestParam(required = false) String searchText, Model model) {
+        model.addAttribute("categoriesData", categoryService.findCategories(searchText));
         return "categoriesPage";
     }
 
--- src/main/java/com/sda/final31/finalapp/categories/CategoryService.java
+++ src/main/java/com/sda/final31/finalapp/categories/CategoryService.java
@@ -2,19 +2,27 @@ package com.sda.final31.finalapp.categories;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
 
-    public List<CategoryDto> findCategories() {
+    public List<CategoryDto> findCategories(String searchText) {
         CategoryDao categoryDao = CategoryDao.getInstance();
         List<Category> categoryList = categoryDao.getCategoryList();
         List<CategoryDto> dtos = categoryList.stream()
                 .map(x -> buildDto(x))
                 .collect(Collectors.toList());
+
+        if (searchText == null) {
+            return dtos;
+        }
+        for (CategoryDto dto : dtos) {
+            if (dto.getCategoryName().toUpperCase().contains(searchText.toUpperCase())) {
+                dto.getState().setSelected(true);
+            }
+        }
         return dtos;
     }
 
